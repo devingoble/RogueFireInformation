46413d8f16991131c322aa245d0bdbeadb15e16d
Devin Goble <devin.goble@outlook.com>
2020-09-10 10:29:48 -0700

    initial commit

---
+++ mfri/src/pages/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <Layout>
+
+  <h1>Unofficial Fire Information</h1>
+  <p>I've been frustrated by how difficult it is to find updated information on the South Obenchain and Almeda fires. This is my attempt to aggregate some sources that I've found helpful. This site isn't endorsed by any agency, or any of the organizations who are linked here. This is essentially a list of my own bookmarks that you may find useful.</p>
+
+  <h2>Public Agencies</h2>
+  <h3>Jackson County</h3>
+  <a href="https://jacksoncountyor.org/emergency/News/News-Information">News & Information</a>
+  <p>This is where the County posts their official news for the fires, things you may need to know, and evacuation areas.</p>
+  <p>Below are some specific links provided by the County for quick reference</p>
+  <a href="https://jcgis.maps.arcgis.com/apps/webappviewer/index.html?id=3caa39804db54631a61007180d5ef415">Live Evacuation Map</a>
+  <p>The live evacuation maps shows current areas that are under evacuation orders. Please note that this map does take a little time to update after official notice has been given, so don't rely on it being real-time. See the News & Information link above for text descriptions of the evacuation zones.</p>
+  <a href="http://arcg.is/014bzS">Missing Family or Friends</a>
+  <p>Fill out this form if you have been unable to contact family or friends. Law enforcement is using this information to know how to prioritize efforts.</p>
+  <a href="http://arcg.is/14vDXC">I am safe</a>
+  <p>Use this form if you have evacuated and have been unable to let family or friends know. Law enforcement is using this information to reunite people.</p>
+  <a href="https://www.facebook.com/jacksoncountyoregonem">Jackson County Emergency Management</a>
+  
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Fire Information'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>

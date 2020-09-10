f498fce74866f5c668332b6fa28ca261e109852c
Devin Goble <devin.goble@outlook.com>
2020-09-10 13:09:29 -0700

    add about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,15 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>About</h1>
+      <p>I've been frustrated by how difficult it is to find updated information on the South Obenchain and Almeda fires. This is my attempt to aggregate some sources that I've found helpful. This site isn't endorsed by any agency, or any of the organizations who are linked here. This is essentially a list of my own bookmarks that you may find useful.</p>
+      <p>If you see an issue, or want to suggest a resource, feel free to send an email to rfisuggestions@outlook.com.</p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: 'About'
   }
 }
 </script>

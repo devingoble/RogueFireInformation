a43ec8819b4d3e11f7e81197181e636c3c81e388
Devin Goble <devin.goble@outlook.com>
2020-09-10 13:01:44 -0700

    update list of links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,7 @@
 
   <h1>Unofficial Fire Information</h1>
   <p>I've been frustrated by how difficult it is to find updated information on the South Obenchain and Almeda fires. This is my attempt to aggregate some sources that I've found helpful. This site isn't endorsed by any agency, or any of the organizations who are linked here. This is essentially a list of my own bookmarks that you may find useful.</p>
+  <p>If you see an issue, or want to suggest a resource, feel free to send an email to rfisuggestions@outlook.com.</p>
 
   <h2>Public Agencies</h2>
   <h3>Jackson County</h3>
@@ -15,10 +16,91 @@
   <p>Fill out this form if you have been unable to contact family or friends. Law enforcement is using this information to know how to prioritize efforts.</p>
   <a href="http://arcg.is/14vDXC">I am safe</a>
   <p>Use this form if you have evacuated and have been unable to let family or friends know. Law enforcement is using this information to reunite people.</p>
+  <a href="https://jacksoncountyor.org/emergency/News/News-Information/citizen-alert-sign-up-today-1">Citizen Alert</a>
+  <p>The County provides a way to sign up for alerts about important information such as evacuations that apply to you.</p>
   <a href="https://www.facebook.com/jacksoncountyoregonem">Jackson County Emergency Management</a>
   <p>The official Facebook page of the Jackson County Emergency Management team.</p>
   <a href="https://twitter.com/JacksonCountyOR">@JacksonCountyOR</a>
   <p>Official tweets from Jackson County</p>
+
+  <h3>Oregon Department of Forestry Southwest Oregon District</h3>
+  <a href="https://www.facebook.com/ODFSouthwest">ODF Facebook</a>
+  <p>ODF Southwest's official Facebook page</p>
+  <a href="https://swofire.com/">SWO Fire</a>
+  <p>ODF Southwest's news site. They post some updates about various fires in their area of operation.</p>
+
+  <h3>US Forest Service</h3>
+  <a href="https://inciweb.nwcg.gov/">InciWeb</a>
+  <p>USFS provides InciWeb to track interagency information. Not all fires appear on here, but you can use it as a rough guide to what they are tracking.</p>
+  <a href="https://fsapps.nwcg.gov/afm/">Active Fire Mapping Program</a>
+  <p>You can get some good satellite imagery from various fires from here.</p>
+
+  <h3>State of Oregon</h3>
+  <a href="https://wildfire.oregon.gov/">Oregon Wildfire Resources</a>
+  <p>The State has a number of information resources</p>
+  <a href="https://www.tripcheck.com/">ODOT TripCheck</a>
+  <p>TripCheck shows road closures, and has links to cameras that you can use to see traffic and even smoke conditions.</p>
+  <a href="https://twitter.com/OregonOEM">Oregon Emergency Management Twitter</a>
+  <p>They post some general information about fires in Oregon.</p>
+  <a href="https://twitter.com/ORStatePolice">Oregon State Police Twitter</a>
+  <p>OSP posts about road closures</p>
+
+  <h3>National Weather Service</h3>
+  <p>Lots of good information about weather, smoke, and air quality.</p>
+  <a href="https://www.weather.gov/mfr/">NWS Medford page</a>
+  <p>The NWS page for the local area</p>
+  <a href="https://twitter.com/NWSMedford">@NWSMedford</a>
+  <p>Twitter account for the local NWS office.
+
+  <h2>News Stations</h2>
+  <h3>KTVL</h3>
+  <a href="https://ktvl.com/">KTVL Website</a>
+  <p>Local TV news</p>
+  <a href="https://ktvl.com/news/local/south-obenchain-fire-embers-ignite-new-fire-near-reese-creek-1000-acres">South Obenchain Story</a>
+  <p>They seem to be updating this article as they get new information.</p>
+  <a href="https://ktvl.com/news/local/grass-fire-near-ashland-water-treatment-plant-threatens-structures-and-homes-evacuations">Almeda Fire Story</a>
+  <p>They seem to be updating this article as they get new information.</p>
+  <a href="https://ktvl.com/weather/cameras">Live Cameras</a>
+  <p>Cameras from various points around the valley.</p>
+  <a href="https://twitter.com/KTVL">@KTVL</a>
+  <p>KTVL Twitter account. They've been good about posting updated information, including weather maps showing smoke.</p>
+  <a href="https://www.facebook.com/KTVLNews10/">KTVL Facebook</a>
+  <p>Official Facebook page</p>
+
+  <h3>KDRV</h3>
+  <a href="http://kdrv.com">KDRV Website</a>
+  <p>Local TV news</p>
+  <a href="https://www.kdrv.com/content/news/New-fire-near-Eagle-Point-estimated-at-1700-acres-572353401.html">South Obenchain Story</a>
+  <p>They seem to be updating this article as they get new information.</p>
+  <a href="https://www.kdrv.com/content/news/Ashland-under-Level-1-evacuation-notice-as-high-winds-stoke-fire-concerns-572349401.html">Almeda Fire Story</a>
+  <p>They seem to be updating this article as they get new information.</p>
+  <a href="https://twitter.com/KDRV">@KDRV</a>
+  <p>KDRV Twitter account. They've been good about posting updated information.</p>
+  <a href="https://www.facebook.com/KDRV12">KDRV Facebook</a>
+  <p>Official Facebook page</p>
+
+  <h3>KOBI</h3>
+  <a href="http://kobi5.com">KOBI Website</a>
+  <p>Local TV news</p>
+  <a href="https://kobi5.com/news/fire-danger-red-fire-season-2020-132697">KOBI Fire covera</a>
+  <p>Various stories related to fires in the area</p>
+  <a href="https://twitter.com/kobitv">@KOBITV</a>
+  <p>KOBI Twitter account. They've been good about posting updated information.</p>
+  <a href="https://facebook.com/kobitv">KOBI Facebook</a>
+  <p>Official Facebook page</p>
+
+  <h2>Community Resources</h2>
+  <p>These are various non-affiliated resources that I've found around the community.</p>
+  <a href="https://twitter.com/MedfordALERT">@MedfordALERT</a>
+  <p>They have been tirelessly posting huge amounts of content aggregated from a number of different resources and scanner traffic.</p>
+  <a href="https://www.facebook.com/RogueWeatherNow/">Rogue Weather</a>
+  <p>They've got relationships with firefighting agencies, and are able to get some pretty good information.</p>
+
+  <h2>Miscellaneous Tools</h2>
+  <a href="https://flightaware.com/">FlighAware</a>
+  <p>It turns out that they have tracking data for the air tankers and helicopters in the valley. Sometimes you can get a better view of where firefighting efforts are concentrated by watching their flight patterns.</p>
+  <a href="https://mappingsupport.com/p2/gissurfer.php?center=40.749596,-111.533203&zoom=5&basemap=USA_basemap&overlay=VIIRS_24_hours,MODIS_24_hours&txtfile=https://mappingsupport.com/p2/special_maps/disaster/USA_wildland_fire.txt">mappingsupport.com</a>
+  <p>This tool gets infrared data from NASA. It's interesting, but be aware that it can be several hours old.</p>
   </Layout>
 </template>
 

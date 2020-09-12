6c35aa6e70f0a59356404f87993cfb82e2195f9d
Devin Goble <devin.goble@outlook.com>
2020-09-12 00:01:19 -0700

    add meta tag

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  head.meta.push({
+    name: 'keywords',
+    content: 'A collection of links to various resources for the Almeda Fire and Obenchain Fire in the Rogue Valley, including Medford, Ashland, Talent, Phoenix, Eagle Point, White City, Shady Cove, and Butte Falls. This includes evacuation notices, public health links, relief efforts, news, and general information.'
+  })
 }
